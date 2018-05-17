17a08f416c0f2f4007c915d6d90d701befe7e0d1
Mohammed <mohammed.bu.mo@gmail.com>
2018-05-18 00:27:03 +0300

    For vue-cli bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
-
+  	<main class="hero is-fullheight is-primary">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-centered">
+					<div class="column is-6 is-flex is-column is-centered">
+						<div class="image keswah-logo">
+							<img src="../assets/img/logo.png" alt="">
+						</div>
+            <Login />
+					</div>
+				</div>
+			</div>
+		</div>
+	</main></template>
+ 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import Login from "@/components/Login.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    Login
+  } 
 })
 export default class Home extends Vue {}
 </script>
+<style lang="scss">
+.keswah-logo {
+  max-width: 15rem;
+  margin-bottom: 5rem;
+}
+.column.is-flex {
+  &.is-column {
+    flex-direction: column;
+  }
+  &.is-centered {
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+</style>
